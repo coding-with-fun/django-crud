2d3c0dbcb120fe60121920e94739cce26c4fd1cb
harsh2124 <harshp2482@gmail.com>
2020-06-04 09:35:09 +0530

    Adding minor changes

--- crudProj/crudApp/urls.py
+++ crudProj/crudApp/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls import url
 from crudApp import views
 from django.urls import path
 
-# app_name = 'crudApp'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    path('projects/', views.project_list, name='project_list'),
+    url(r'^projects/$', views.project_list, name='project_list'),
+    url(r'^new/$', views.project_create, name='project_new'),
+
     path('view/<str:slug>', views.project_view, name='project_view'),
-    path('new', views.project_create, name='project_new'),
     path('edit/<str:slug>', views.project_update, name='project_edit'),
     path('delete/<str:slug>', views.project_delete, name='project_delete'),
 ]
--- crudProj/crudProj/urls.py
+++ crudProj/crudProj/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     url(r'^register/$', views.register, name='register'),
     url(r'^user_login/$', views.user_login, name='user_login'),
     url(r'^logout/$', views.user_logout, name='logout'),
-    url(r'', include('crudApp.urls')),
+    url(r'^', include('crudApp.urls')),
 ]
 
 if settings.DEBUG:
